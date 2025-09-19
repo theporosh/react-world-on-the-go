4a1201ae030a0f0a1678ac85573fbb938a0032a2
theporosh <theporosh@gmail.com>
2025-09-19 14:09:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  
+return (
+    <>
+     <Countries></Countries>
+    </>
+  )
+}
+
+export default App
